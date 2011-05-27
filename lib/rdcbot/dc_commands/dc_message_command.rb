4cd1ba365d6f40171416e22651b1ba7e63bd57a3
Andrey Sidorov <takandar@gmail.com>
2011-05-28 01:12:45 +0400

    update message command. added better example in readme

--- lib/rdcbot/dc_commands/dc_message_command.rb
+++ lib/rdcbot/dc_commands/dc_message_command.rb
@@ -1,7 +1,11 @@
 class DCMessageCommand < DCCommand
+  attr_accessor :user
+  attr_accessor :text
+
   def initialize(user, data)
     @user = user
-    @data = Iconv.iconv('utf-8', 'cp1251', data)
+    @data = data
+    @text = Iconv.iconv('utf-8', 'cp1251', data)
   end
 
   def to_s
