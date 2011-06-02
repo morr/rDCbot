6709dc6fb53cbdd658d39221a9713b76403c22db
Andrey Sidorov <takandar@gmail.com>
2011-06-02 12:23:28 +0400

    changed regexp for name command match

--- lib/rdcbot/dc_command_builder.rb
+++ lib/rdcbot/dc_command_builder.rb
@@ -3,14 +3,13 @@ class DCCommandBuilder
     name, data = parse(*args)
 
     begin
-      if name =~ /<(.*)>/
+      if name =~ /<([^>]+)>/
         DCMessageCommand.new($1, data)
       else
         Object::const_get("DC#{name}Command").new(data)
       end
     rescue NameError => e
       DCUnknownCommand.new(name, data)
-      #raise "unknown command: #{name}"
     end
   end
 
